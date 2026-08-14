f275d99c415d27de26e903394ac3d17817abe32e
Najmus Sakib <sakibthoha5@gmail.com>
2026-08-14 11:04:28 +0600

    mini

--- custom_nn.py
+++ custom_nn.py
@@ -59,3 +59,54 @@ customModel = myNN()
 customModel.fit(X_train_scaled, y_train, epochs=8000, loss_thresold=0.4631)
 
 customModel.predict(X_test_scaled)
+
+
+# mini batch
+def mini_batch_gradient_descent(X, y_true, epochs = 100, batch_size = 5, learning_rate = 0.01):
+    
+    number_of_features = X.shape[1]
+    # numpy array with 1 row and columns equal to number of features. In 
+    # our case number_of_features = 3 (area, bedroom and age)
+    w = np.ones(shape=(number_of_features)) 
+    b = 0
+    total_samples = X.shape[0] # number of rows in X
+    
+    if batch_size > total_samples: # In this case mini batch becomes same as batch gradient descent
+        batch_size = total_samples
+        
+    cost_list = []
+    epoch_list = []
+    
+    num_batches = int(total_samples/batch_size)
+    
+    for i in range(epochs):    
+        random_indices = np.random.permutation(total_samples)
+        X_tmp = X[random_indices]
+        y_tmp = y_true[random_indices]
+        
+        for j in range(0,total_samples,batch_size):
+            Xj = X_tmp[j:j+batch_size]
+            yj = y_tmp[j:j+batch_size]
+            y_predicted = np.dot(w, Xj.T) + b
+            
+            w_grad = -(2/len(Xj))*(Xj.T.dot(yj-y_predicted))
+            b_grad = -(2/len(Xj))*np.sum(yj-y_predicted)
+            
+            w = w - learning_rate * w_grad
+            b = b - learning_rate * b_grad
+                
+            cost = np.mean(np.square(yj-y_predicted)) # MSE (Mean Squared Error)
+        
+        if i%10==0:
+            cost_list.append(cost)
+            epoch_list.append(i)
+        
+    return w, b, cost, cost_list, epoch_list
+
+w, b, cost, cost_list, epoch_list = mini_batch_gradient_descent(
+    scaled_X,
+    scaled_y.reshape(scaled_y.shape[0],),
+    epochs = 120,
+    batch_size = 5
+)
+w, b, cost
