8709ae05ddcb9a2428ef6b1df899f790229fc920
Najmus Sakib <sakibthoha5@gmail.com>
2026-08-14 10:53:55 +0600

    stochastic

---
+++ stochastic.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("homeprices_banglore.csv")
+
+# scaling
+from sklearn import preprocessing
+sx = preprocessing.MinMaxScaler()
+sy = preprocessing.MinMaxScaler()
+
+scaled_X = sx.fit_transform(df.drop('price',axis='columns'))
+scaled_y = sy.fit_transform(df['price'].values.reshape(df.shape[0],1))
+
+# batch grad descent
+def batch_gradient_descent(X, y_true, epochs, learning_rate = 0.01):
+
+    number_of_features = X.shape[1]
+    # numpy array with 1 row and columns equal to number of features. In 
+    # our case number_of_features = 2 (area, bedroom)
+    w = np.ones(shape=(number_of_features)) 
+    b = 0
+    total_samples = X.shape[0] # number of rows in X
+    
+    cost_list = []
+    epoch_list = []
+    
+    for i in range(epochs):        
+        y_predicted = np.dot(w, X.T) + b
+
+        w_grad = -(2/total_samples)*(X.T.dot(y_true-y_predicted))
+        b_grad = -(2/total_samples)*np.sum(y_true-y_predicted)
+        
+        w = w - learning_rate * w_grad
+        b = b - learning_rate * b_grad
+        
+        cost = np.mean(np.square(y_true-y_predicted)) # MSE (Mean Squared Error)
+        
+        if i%10==0:
+            cost_list.append(cost)
+            epoch_list.append(i)
+        
+    return w, b, cost, cost_list, epoch_list
+
+w, b, cost, cost_list, epoch_list = batch_gradient_descent(scaled_X,scaled_y.reshape(scaled_y.shape[0],),500)
+w, b, cost
+
+
+def predict(area,bedrooms,w,b):
+    scaled_X = sx.transform([[area, bedrooms]])[0]
+    # here w1 = w[0] , w2 = w[1], w3 = w[2] and bias is b
+    # equation for price is w1*area + w2*bedrooms + w3*age + bias
+    # scaled_X[0] is area
+    # scaled_X[1] is bedrooms
+    # scaled_X[2] is age
+    scaled_price = w[0] * scaled_X[0] + w[1] * scaled_X[1] + b
+    # once we get price prediction we need to to rescal it back to original value
+    # also since it returns 2D array, to get single value we need to do value[0][0]
+    return sy.inverse_transform([[scaled_price]])[0][0]
+
+predict(2600,4,w,b)
+
+def stochastic_gradient_descent(X, y_true, epochs, learning_rate = 0.01):
+ 
+    number_of_features = X.shape[1]
+    # numpy array with 1 row and columns equal to number of features. In 
+    # our case number_of_features = 3 (area, bedroom and age)
+    w = np.ones(shape=(number_of_features)) 
+    b = 0
+    total_samples = X.shape[0]
+    
+    cost_list = []
+    epoch_list = []
+    
+    for i in range(epochs):    
+        random_index = random.randint(0,total_samples-1) # random index from total samples
+        sample_x = X[random_index]
+        sample_y = y_true[random_index]
+        
+        y_predicted = np.dot(w, sample_x.T) + b
+    
+        w_grad = -(2/total_samples)*(sample_x.T.dot(sample_y-y_predicted))
+        b_grad = -(2/total_samples)*(sample_y-y_predicted)
+        
+        w = w - learning_rate * w_grad
+        b = b - learning_rate * b_grad
+        
+        cost = np.square(sample_y-y_predicted)
+        
+        if i%100==0: # at every 100th iteration record the cost and epoch value
+            cost_list.append(cost)
+            epoch_list.append(i)
+        
+    return w, b, cost, cost_list, epoch_list
+
+w_sgd, b_sgd, cost_sgd, cost_list_sgd, epoch_list_sgd = SGD(scaled_X,scaled_y.reshape(scaled_y.shape[0],),10000)
+w_sgd, b_sgd, cost_sgd
